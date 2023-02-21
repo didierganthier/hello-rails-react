a1164c69a680bea055aa38f1e201d2290607c809
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-20 19:54:29 -0500

    Configure store

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,10 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import App from "./App";
 
 ReactDOM.render(
   <App/>,
