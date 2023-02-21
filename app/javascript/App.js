0e1c661031144fc38d7cf698d5dc36d2e7ecc52d
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-20 21:03:02 -0500

    Connect store/create routes

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {Link, Route, Routes} from "react-router-dom";
+import Greeting from "./pages/Greeting";
 
-const  App = () => {
+const App = () => {
   return <>
     <h1>Hello World!</h1>
+    <Link to="/greeting">Greeting page</Link>
+    <Routes>
+      <Route path="/greeting" element={<Greeting/>}/>
+    </Routes>
   </>
 }
 
