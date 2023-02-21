235e6bb04f77cebeae64f717b86340e6b93a9c60
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-02-21 06:50:17 -0500

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_223532) do
   enable_extension "plpgsql"
 
   create_table "greetings", force: :cascade do |t|
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
